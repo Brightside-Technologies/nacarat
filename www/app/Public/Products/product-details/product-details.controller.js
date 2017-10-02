e67bb6634d75e3384da081ced0b9f7f0847964a8
diegob <diego91bernal@gmail.com>
2017-10-01 21:24:28 -0700

    styled products cards in carousel. designed product details page

--- www/app/Public/Products/product-details/product-details.controller.js
+++ www/app/Public/Products/product-details/product-details.controller.js
@@ -1,14 +1,49 @@
-export default function ProductDetailsController($state, $stateParams, ProductsService) {
-  var productId = $stateParams.productId;
-  var vm = this;
-  vm.product = {};
+// TODO: Implement carousel for product images
+// TODO: Do I need a helper for Flickity? Or do I need to refactor only?
+// TODO: Need a default image/icon for vendors with no logo
+// TODO: Need to add street number for address in product.seller.address
+export default function ProductDetailsController($scope, $timeout, $state, $stateParams, $document, FlickityService, ProductsService) {
+    var productId = $stateParams.productId;
+    var vm = this;
+    vm.productObject = {};
+    var productImagesCarouselOptions = {
+        cellSelector: '.nacarat-carousel__slide',
+        wrapAround: true,
+        freeScrollFriction: 0.03
+    };
 
-  init();
+    init();
 
-  function init() {
-    ProductsService.get(productId)
-      .then(function(product) {
-        vm.product = product.data;
-      });
-  }
+    function init() {
+        // wait until document has loaded
+        angular.element($document[0]).ready(function() {
+            ProductsService.get(productId)
+                .then(function(product) {
+                        vm.productObject = product.data;
+                        return vm.productObject;
+                    },
+                    function error(err) {
+                        console.log("err", err)
+                    })
+                .then(function(productObject) {
+                    // find DOM elements with which to instantiate flickity instances
+                    var productImagesCarousel = angular.element(document.getElementById('product-images-carousel'));
+                    initCarouselInstance(productImagesCarousel);
+                });
+        });
+    }
+
+    function initCarouselInstance(carousel) {
+        $timeout(function() {
+
+            // create flickity instances
+            FlickityService.create(carousel[0], carousel[0].id, productImagesCarouselOptions);
+
+            // when scope is destroyed, destroy flickity instances
+            $scope.$on('$destroy', function() {
+                FlickityService.destroy(carousel[0].id);
+            });
+
+        });
+    }
 }
